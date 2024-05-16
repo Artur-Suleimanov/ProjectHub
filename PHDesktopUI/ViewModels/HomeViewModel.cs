315d5a4cf39794a6e4177cc5aeb4bf42557e04d8
Артур Сулейманов <artur.suleimanov1997@gmail.com>
2024-05-16 14:53:25 +0400

    Add Home page

---
+++ PHDesktopUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHDesktopUI.ViewModels
+{
+    public class HomeViewModel : Screen
+    {
+        // Sales Page Creation - A TimCo Retail Manager Video
+
+        private BindingList<string> _projects;
+
+        public BindingList<string> Projects
+        {
+            get { return _projects; }
+            set
+            { 
+                _projects = value; 
+                NotifyOfPropertyChange(() => Projects);
+            }
+        }
+
+        private string _projectName;
+
+        public string ProjectName
+        {
+            get { return _projectName; }
+            set 
+            { 
+                _projectName = value;
+                NotifyOfPropertyChange(() => Projects);
+            }
+        }
+
+        private string _projectDescription;
+
+        public string ProjectDescription
+        {
+            get { return _projectDescription; }
+            set 
+            {
+                _projectDescription = value;
+                NotifyOfPropertyChange(() => Projects);
+            }
+        }
+
+        private BindingList<string> _tasks;
+
+        public BindingList<string> Tasks
+        {
+            get { return _tasks; }
+            set 
+            { 
+                _tasks = value; 
+            }
+        }
+
+    }
+}
